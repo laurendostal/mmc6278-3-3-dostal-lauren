f5400b1dc1c5fb769e4d8aa553119c2fbc4e21c9
Lauren <laurendostal@yahoo.com>
2024-06-02 17:29:11 -0400

    modify app.js, add env file

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+
 // TODO: import the getCityInfo and getJobs functions from util.js
+const findJobUtils = require('./util')
 
 // TODO: Statically serve the public folder
+app.use(express.static('public'))
 
 // TODO: declare the GET route /api/city/:city
 // This endpoint should call getCityInfo and getJobs and return
@@ -13,5 +16,19 @@ const app = express()
 // jobs (with value of the getJobs function)
 // If no city info or jobs are found,
 // the endpoint should return a 404 status
+app.get('/api/city/:city', async (req, res) => {
+    const city = req.params.city
+    try {
+        const cityInfo = await findJobUtils.getCityInfo(city)
+        const jobsInfo = await findJobUtils.getJobs(city)
+        if (cityInfo || jobsInfo) {
+            return res.json({cityInfo: cityInfo, jobs: jobsInfo})
+        } else {
+            return res.status(404).json({error: 'No city info and job found'})
+        }
+    } catch(err) {
+        return res.status(404).json({error: err.message})
+    }
+})
 
 module.exports = app
